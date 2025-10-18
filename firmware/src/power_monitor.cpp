e166364685bc63fa9c8a1d871364336812160141
Vedant Misra <81390906+vedm-industries@users.noreply.github.com>
2025-10-18 12:34:51 -0500

    baseline power_monitor

--- firmware/src/power_monitor.cpp
+++ firmware/src/power_monitor.cpp
@@ -1,1 +1,19 @@
+#include <Adafruit_INA219.h>
+
+extern Adafruit_INA219 ina219; // Access object from main
+
+void setupPowerMonitor() {
+  if (!ina219.begin()) {
+    Serial.println("Failed to find INA219 sensor. Check wiring!");
+    while (1);
+  } else {
+    Serial.println("INA219 initialized successfully.");
+  }
+}
+
+void readPower(float &voltage, float &current, float &power) {
+  voltage = ina219.getBusVoltage_V();
+  current = ina219.getCurrent_mA();
+  power = ina219.getPower_mW();
+}
 
